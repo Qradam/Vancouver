10575b087de3e6a7bd16963d268f739e593e0087
William <williamcraske@gmail.com>
2025-09-16 20:38:10 -0700

    finished mobile friendly on rest of components, except for the updates

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
 
-  const navItems = [{name: 'HOME', href: '#hero'}, {name: 'TEAM', href: '#characterCards'}, {name: 'ABOUT US', href: '#aboutUs'}, {name: 'SOCIAL MEDIA', href: '#socialMedia'}, {name: 'CONTACT', href: '#footer'}];
+  const navItems = [{name: 'HOME', href: '#hero'}, {name: 'SOCIAL MEDIA', href: '#socialMedia'}, {name: 'TEAM', href: '#characterCards'}, {name: 'ABOUT US', href: '#aboutUs'}, {name: 'CONTACT', href: '#footer'}];
 
   return (
     <footer className="py-8 px-6 bg-gray-900/50 border-t border-gray-800">
@@ -11,23 +11,11 @@ export default function Footer() {
           <div className="flex items-center space-x-4">
           <img src="/logo.svg" alt="League" className="" width={48} height={48}/>
             <p className="text-gray-300 text-sm max-w-md">
-              Lorem ipsum dolor sit amet consectetur. Congue nulla sed turpis vulputate sit quam. 
-              Dis sit etiam vestibulum sit laoreet. Dui risus odio arcu venenatis lobortis malesuada in eleifend.
+              Vancouver Impact Canadian Esports Organization
             </p>
           </div>
           
-          <nav className="flex space-x-8">
-          {navItems.map((item) => (
-          <Link
-            key={item.name}
-            href={item.href}
-            scroll={true}
-            className="text-white uppercase font-medium hover:text-pink-400 transition-colors duration-300 hover:scale-105 transform"
-          >
-            {item.name}
-          </Link>
-        ))}
-          </nav>
+        
         </div>
       </div>
     </footer>
